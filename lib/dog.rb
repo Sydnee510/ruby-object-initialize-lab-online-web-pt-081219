8bf0c2b9471970022fe49a0dbcd394f327b0c2d5
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-28 04:21:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog 
   def initialize(name,breed)
     @name = name 
-    @breed = breed
+    @breed = breed = "Mutt"
  end
- def breed 
-  @breed = 'Mutt' 
- end
+# def breed 
+#   @breed = 'Mutt' 
+# end
  end
  
 # def name=(name)
